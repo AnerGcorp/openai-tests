c2c6cbe40acf588ccbf1f83604eddb4bf226ff14
Anergcorp <ahunatajanov@gmail.com>
2024-03-20 01:03:07 +0200

    Splitting text into chunks

--- facts/main.py
+++ facts/main.py
@@ -1,9 +1,24 @@
 from langchain_community.document_loaders.text import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
 from dotenv import load_dotenv
 
 load_dotenv()
 
-loader = TextLoader("facts.txt")
-docs = loader.load()
+# initializing Openai embeddings
+embeddings = OpenAIEmbeddings()
 
-print(docs)
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=200,
+    chunk_overlap=0
+)
+
+loader = TextLoader("facts.txt")
+docs = loader.load_and_split(
+    text_splitter=text_splitter
+)
+
+for doc in docs:
+    print(doc.page_content)
+    print("\n")
