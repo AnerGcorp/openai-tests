49396b03381979cfbedc0ca8bd820f6dbd2646d9
Anergcorp <ahunatajanov@gmail.com>
2024-03-20 00:01:30 +0200

    adding memory support to LLM

--- tchat/main.py
+++ tchat/main.py
@@ -1,22 +1,29 @@
 from langchain_openai import ChatOpenAI
-from langchain.chains import LLMChain
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.chains.llm import LLMChain
+from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# initializing ChatOpenAI
 chat = ChatOpenAI()
-
+# adding memory to store the chat history
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+# chat prompt
 prompt = ChatPromptTemplate(
-    input_variables=["content"],
+    input_variables=["content", "messages"],
     messages=[
+        MessagesPlaceholder(variable_name="messages"),
         HumanMessagePromptTemplate.from_template("{content}")
     ]
 )
 
+# chaining
 chain = LLMChain(
     llm=chat,
-    prompt=prompt
+    prompt=prompt,
+    memory=memory
 )
 
 while True:
