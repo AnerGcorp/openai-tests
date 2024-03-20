3bdde9e5c8924e9fbf37a42bb42084aaf8daa6d3
Anergcorp <ahunatajanov@gmail.com>
2024-03-20 21:24:17 +0200

    using function agent and agent executor

---
+++ agents/main.py
@@ -0,0 +1,37 @@
+from langchain_openai.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder
+)
+from langchain.agents.openai_functions_agent import OpenAIFunctionsAgent, AgentExecutor
+from dotenv import load_dotenv
+
+from tools.sql import run_query_tool
+
+load_dotenv()
+
+chat = ChatOpenAI()
+
+prompt = ChatPromptTemplate(
+    messages=[
+        HumanMessagePromptTemplate.from_template("{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad")
+    ]
+)
+
+tools = [run_query_tool]
+
+agent = OpenAIFunctionsAgent(
+    llm=chat,
+    prompt=prompt,
+    tools=tools
+)
+
+agent_executor = AgentExecutor(
+    agent=agent,
+    verbose=True,
+    tools=tools
+)
+
+agent_executor.invoke("How many users in the database? ")
