772ba4c858a46ad5c48e3423a8883af074d60f92
Anergcorp <ahunatajanov@gmail.com>
2024-03-20 16:44:14 +0200

    removing dublicates with redundant filter

--- facts/prompt.py
+++ facts/prompt.py
@@ -3,6 +3,7 @@ from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores.chroma import Chroma
 from langchain.chains.retrieval_qa.base import RetrievalQA
 from dotenv import load_dotenv
+from redundant_filter_retriever import RedundantFilterRetriever
 
 load_dotenv()
 
@@ -12,7 +13,10 @@ db = Chroma(
     persist_directory="emb",
     embedding_function=embeddings
 )
-retriever = db.as_retriever()
+retriever = RedundantFilterRetriever(
+    chroma=db,
+    embeddings=embeddings
+)
 
 chain = RetrievalQA.from_chain_type(
     llm=chat,
